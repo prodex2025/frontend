a8b798d2f7478de0a57329a4043ad30479bd3a46
okmyk <mikoto02@outlook.jp>
2025-06-30 16:27:36 +0900

    ログイン画面を作成

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,12 +4,27 @@ import styles from './page.module.css';
 
 export default function LoginPage() {
   return (
-    <div className={styles.login}>
-      <h2>ログイン</h2>
-      <label htmlFor="userId">ユーザーID</label>
-      <input className={styles.input} type="text" name='userId' id='userId'/>
-      <label htmlFor="password">パスワード</label>
-      <input className={styles.input}  type="password" name='password' id='password' />
+    <div className={styles.div}>
+      <form className={styles.loginForm}>
+        <h2 className={styles.h2}>ログイン</h2>
+        <div className={styles.inputContent}>
+          <input className={styles.content} type="text" name='userId' id='userId' placeholder='' required/>
+          <label htmlFor='userId'>ID</label>
+        </div>
+        <div className={styles.inputContent}>
+          <input className={styles.content} type="password" name='password' id='password' placeholder='' required/>
+          <label htmlFor='password'>パスワード</label>
+        </div>
+        <LoginBtn/>
+      </form>
+      <p>アカウントが未登録ですか？</p>
+      <a href={"/"}>アカウント作成</a>
     </div>
+  );
+}
+
+function LoginBtn(){
+  return(
+    <button type='submit' className={styles.loginBtn}>ログイン</button>
   );
 }
