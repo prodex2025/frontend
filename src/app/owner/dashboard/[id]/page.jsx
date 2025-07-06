eb85dfc4840fa3fe42b0476351cad7f4ec04eeaf
okmyk <mikoto02@outlook.jp>
2025-07-06 13:45:51 +0900

    経営者のトップ画面のヘッダーを作成

--- src/app/owner/dashboard/[id]/page.jsx
+++ src/app/owner/dashboard/[id]/page.jsx
@@ -4,13 +4,11 @@ import styles from './page.module.css';
 
 export default function LoginPage() {
   return (
-    <div className={styles.login}>
-      <h2>ログイン</h2>
-      <label htmlFor="userId">ユーザーID</label>
-      <input className={styles.input} type="text" name='userId' id='userId'/>
-      <label htmlFor="password">パスワード</label>
-      <input className={styles.input}  type="password" name='password' id='password' />
-      
+    <div>
+      <div className={styles.heder}>
+        <h1>登録店舗一覧</h1>
+        <button type='button' value="店舗追加"></button>
+      </div>
     </div>
   );
 }
