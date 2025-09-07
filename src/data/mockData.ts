68427f31f45055506a833d89c6e9c33bc9aac8ed
okmyk <mikoto02@outlook.jp>
2025-09-07 23:03:49 +0900

    3Dテストモデルを追加

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -326,7 +326,7 @@ export const dishes:Dishes[] = [
     price:420,
     description:'パスタ',
     image_url:'@/image/pasuta.png',
-    video_url:'@/video/menu.mp4',
+    video_url:'40c245d593a44e91ad97d81828e9cc64',
   },
   {
     id:3,
